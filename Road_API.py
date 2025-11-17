683643c62da34641f0b4cc945e431527e9d9ee30
Jarvasio <149535389+Jarvasio@users.noreply.github.com>
2025-11-17 12:41:25 +0000

    Update Road_API.py

--- Road_API.py
+++ Road_API.py
@@ -186,4 +186,5 @@ async def get_distancias(
     return csv_buffer.getvalue()
 
 if __name__ == "__main__":
-    uvicorn.run("Bapi:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("api:app", host="127.0.0.1", port=8000, reload=True)
+
